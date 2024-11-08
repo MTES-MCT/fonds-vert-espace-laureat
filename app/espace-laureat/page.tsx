cbcf60014eb4b9f05dda9f34710703bc97e69857
Cédric Soulas <soulascedric@gmail.com>
2024-11-08 13:49:43 +0100

    feat: display connected email, redirect otherwise

--- app/espace-laureat/page.tsx
+++ app/espace-laureat/page.tsx
@@ -1,7 +1,18 @@
-export default function EspaceLaureat() {
+import { redirect } from "next/navigation";
+
+import { getSession } from "@/utils/session";
+
+export default async function EspaceLaureat() {
+  const session = await getSession();
+
+  if (!session?.user?.email) {
+    return redirect("/connexion");
+  }
+
   return (
     <div className="max-w-2xl pb-24">
       <h1>Espace lauréat</h1>
+      <p>{session.user.email}</p>
       <p className="fr-text--lead">
         Suivez vos subventions et réalisez une demande de versement.
       </p>
