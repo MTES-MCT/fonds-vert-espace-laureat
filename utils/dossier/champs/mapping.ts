f3c609e6f926fb3c35e8696a7bca0ca1d9bf87f6
Cédric Soulas <soulascedric@gmail.com>
2024-11-15 21:05:40 +0100

    feat: add additional demarche field descriptor ids

--- utils/dossier/champs/mapping.ts
+++ utils/dossier/champs/mapping.ts
@@ -1,11 +1,20 @@
-export const mapping: { [key: string]: string } = {
-  "Q2hhbXAtMjk3MTQ0NA==": "intituleProjet",
-  "Q2hhbXAtMjk5Nzg3Mw==": "resumeProjet",
-  "Q2hhbXAtMzM0ODkyMw==": "departementImplantation",
-  "Q2hhbXAtMzgwNTc1Mw==": "emailRepresentantLegal",
-  "Q2hhbXAtMjkzNDQwMA==": "emailResponsableSuivi",
-  "Q2hhbXAtMTk5MDI2Ng==": "montantSubventionAttribuee",
-  "Q2hhbXAtMjk4MjUwMQ==": "numeroEngagementJuridique",
-  "Q2hhbXAtNDA4MzY4NA==": "autresNumerosEngagementJuridique",
-  "Q2hhbXAtMjk4MjQ5NA==": "dateSignatureDecision",
+const descriptorIds = {
+  intituleProjet: ["Q2hhbXAtMjk3MTQ0NA==", "Q2hhbXAtMjk3ODA3NQ=="],
+  resumeProjet: ["Q2hhbXAtMjk5Nzg3Mw==", "Q2hhbXAtMjY2NDgxNg=="],
+  departementImplantation: ["Q2hhbXAtMzM0ODkyMw==", "Q2hhbXAtMzM0ODgzMg=="],
+  emailRepresentantLegal: ["Q2hhbXAtMzgwNTc1Mw==", "Q2hhbXAtMzgwMzkxMA=="],
+  emailResponsableSuivi: ["Q2hhbXAtMjkzNDQwMA==", "Q2hhbXAtMjkzNDQwMA=="],
+  montantSubventionAttribuee: ["Q2hhbXAtMTk5MDI2Ng==", "Q2hhbXAtMjk4NjgwNA=="],
+  numeroEngagementJuridique: ["Q2hhbXAtMjk4MjUwMQ==", "Q2hhbXAtMjk4NjgxMQ=="],
+  autresNumerosEngagementJuridique: [
+    "Q2hhbXAtNDA4MzY4NA==",
+    "Q2hhbXAtNDA4MzgwNw==",
+  ],
+  dateSignatureDecision: ["Q2hhbXAtMjk4MjQ5NA==", "Q2hhbXAtMjk4NjgwMg=="],
 };
+
+export const mapping: { [key: string]: string } = Object.fromEntries(
+  Object.entries(descriptorIds).flatMap(([key, values]) =>
+    values.map((value) => [value, key]),
+  ),
+);
