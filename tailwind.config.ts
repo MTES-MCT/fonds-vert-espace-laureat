ae318c0562b19e10bf613474c182527c4fa8601a
Cédric Soulas <soulascedric@gmail.com>
2024-11-27 11:08:33 +0100

    feat: use dsfr gray colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,16 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "blue-france-sun-113": "#000091",
+        gray: {
+          100: "#F6F6F6",
+          200: "#E5E5E5",
+          300: "#DDDDDD",
+          400: "#929292",
+          500: "#666666",
+          700: "#3A3A3A",
+          900: "#161616",
+        },
       },
     },
   },
