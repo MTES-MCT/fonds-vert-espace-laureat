ef70260aaeaca1e136a75ddb6500ac90664c8e36
Cédric Soulas <soulascedric@gmail.com>
2024-11-27 15:54:55 +0100

    chore: move projet to own component

---
+++ app/espace-laureat/_components/Projet.tsx
@@ -0,0 +1,58 @@
+import Alert from "@codegouvfr/react-dsfr/Alert";
+
+import { getDossier } from "@/app/espace-laureat/_components/getDossier";
+import { Dossier } from "@/app/espace-laureat/_components/projet/Dossier";
+import { Subvention } from "@/app/espace-laureat/_components/projet/Subvention";
+import {
+  demoStaticDossierNumber,
+  getDemoStaticDossierResponse,
+} from "@/utils/demo";
+
+export async function Projet({ dossierNumber }: { dossierNumber: number }) {
+  const dossierResult =
+    dossierNumber === demoStaticDossierNumber
+      ? getDemoStaticDossierResponse()
+      : await getDossier(dossierNumber);
+
+  if (!dossierResult.success) {
+    return (
+      <div className="max-w-xl">
+        <Alert
+          title="Impossible d'afficher ce dossier"
+          description={dossierResult.error}
+          severity="error"
+        />
+      </div>
+    );
+  }
+
+  const dossier = dossierResult.data;
+
+  return (
+    <>
+      <h1>Projet lauréat</h1>
+      <div className="grid lg:grid-cols-2 gap-y-6 mb-24 border py-6">
+        <Dossier
+          titreDemarche={dossier.demarche.title}
+          dateTraitement={dossier.dateTraitement}
+          dateSignatureDecision={dossier.champs.dateSignatureDecision}
+          montantSubventionAttribuee={dossier.champs.montantSubventionAttribuee}
+          numeroDossierDemarchesSimplifiees={dossier.numero}
+          emailRepresentantLegal={dossier.champs.emailRepresentantLegal}
+          emailResponsableSuivi={dossier.champs.emailResponsableSuivi}
+        />
+        <Subvention
+          intitule={dossier.champs.intituleProjet}
+          resume={dossier.champs.resumeProjet}
+          departementImplantation={dossier.champs.departementImplantation}
+          numeroDossierAgenceEau={dossier.champs.numeroDossierAgenceEau}
+          numeroEngagementJuridique={dossier.champs.numeroEngagementJuridique}
+          autresNumerosEngagementJuridique={
+            dossier.champs.autresNumerosEngagementJuridique
+          }
+          montantSubventionAttribuee={dossier.champs.montantSubventionAttribuee}
+        />
+      </div>
+    </>
+  );
+}
--- app/espace-laureat/page.tsx
+++ app/espace-laureat/page.tsx
@@ -1,16 +1,8 @@
-import Alert from "@codegouvfr/react-dsfr/Alert";
 import { redirect } from "next/navigation";
 
-import { Dossier } from "@/app/espace-laureat/_components/Dossier";
-import { Subvention } from "@/app/espace-laureat/_components/Subvention";
-import {
-  demoStaticDossierNumber,
-  getDemoDossierNumber,
-  getDemoStaticDossierResponse,
-} from "@/utils/demo";
+import { Projet } from "@/app/espace-laureat/_components/Projet";
+import { getDemoDossierNumber } from "@/utils/demo";
 import { getSession } from "@/utils/session";
-
-import { getDossier } from "./_components/getDossier";
 
 export default async function EspaceLaureat() {
   const session = await getSession();
@@ -22,50 +14,5 @@ export default async function EspaceLaureat() {
     return redirect("/connexion");
   }
 
-  const dossierResult =
-    demoDossierNumber === demoStaticDossierNumber
-      ? getDemoStaticDossierResponse()
-      : await getDossier(demoDossierNumber);
-
-  if (!dossierResult.success) {
-    return (
-      <div className="max-w-xl">
-        <Alert
-          title="Impossible d'afficher ce dossier"
-          description={dossierResult.error}
-          severity="error"
-        />
-      </div>
-    );
-  }
-
-  const dossier = dossierResult.data;
-
-  return (
-    <>
-      <h1>Projet lauréat</h1>
-      <div className="grid lg:grid-cols-2 gap-y-6 mb-24 border py-6">
-        <Dossier
-          titreDemarche={dossier.demarche.title}
-          dateTraitement={dossier.dateTraitement}
-          dateSignatureDecision={dossier.champs.dateSignatureDecision}
-          montantSubventionAttribuee={dossier.champs.montantSubventionAttribuee}
-          numeroDossierDemarchesSimplifiees={dossier.numero}
-          emailRepresentantLegal={dossier.champs.emailRepresentantLegal}
-          emailResponsableSuivi={dossier.champs.emailResponsableSuivi}
-        />
-        <Subvention
-          intitule={dossier.champs.intituleProjet}
-          resume={dossier.champs.resumeProjet}
-          departementImplantation={dossier.champs.departementImplantation}
-          numeroDossierAgenceEau={dossier.champs.numeroDossierAgenceEau}
-          numeroEngagementJuridique={dossier.champs.numeroEngagementJuridique}
-          autresNumerosEngagementJuridique={
-            dossier.champs.autresNumerosEngagementJuridique
-          }
-          montantSubventionAttribuee={dossier.champs.montantSubventionAttribuee}
-        />
-      </div>
-    </>
-  );
+  return <Projet dossierNumber={demoDossierNumber} />;
 }
