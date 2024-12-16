049e8bfe72ebf6c3ad67bce30e0e60c501a09713
Cédric Soulas <soulascedric@gmail.com>
2024-12-16 21:46:08 +0100

    feat: improve visual grouping of projets by demarche

--- app/projets/_components/ProjetsPanel.tsx
+++ app/projets/_components/ProjetsPanel.tsx
@@ -23,7 +23,7 @@ export function ProjetsPanel({
       aria-labelledby={`tabpanel-${annee}`}
       tabIndex={0}
     >
-      <ul className="flex flex-wrap justify-start items-end gap-y-12 gap-x-8 bg-gray-100 list-none p-10 mt-0 mb-24">
+      <ul className="flex flex-wrap justify-start items-end gap-y-8 gap-x-8 list-none p-0 m-0">
         {projetsParDemarches?.map((projets: Projets, index: number) => (
           <DemarcheProjets key={index.toString()} projets={projets} />
         ))}
@@ -34,10 +34,10 @@ export function ProjetsPanel({
 
 function DemarcheProjets({ key, projets }: { key: string; projets: Projets }) {
   return (
-    <li key={key} className="p-0 m-0">
-      <h3 className="w-80 mt-0 mb-5 leading-tight text-base text-balance font-medium">
+    <li key={key} className="p-10 mt-0 w-full bg-gray-100">
+      <h2 className="mt-0 mb-5 leading-tight text-xl text-balance font-semibold">
         {projets.demarche_title}
-      </h3>
+      </h2>
       <ul className="list-none flex flex-wrap p-0 m-0 gap-8">
         {projets.projets.map((projet: Projet, index: number) => (
           <ProjetSection key={index.toString()} projet={projet} />
