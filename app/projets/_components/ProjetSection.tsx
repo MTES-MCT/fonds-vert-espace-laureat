b1f97f2868612fb3a08832329bd7130935d0879f
Cédric Soulas <soulascedric@gmail.com>
2025-01-17 15:10:19 +0100

    feat: display ratio on project sections

--- app/projets/_components/ProjetSection.tsx
+++ app/projets/_components/ProjetSection.tsx
@@ -28,25 +28,37 @@ export function ProjetSection({
     currencyOptions,
   ).format(projet.total_des_depenses);
 
+  const ratio =
+    projet.montant_subvention_attribuee &&
+    Math.ceil(
+      (100 * projet.montant_subvention_attribuee) / projet.total_des_depenses,
+    );
+
   return (
     <li
       key={key}
-      className="shadow-xl border-t-4 border-green-400 flex flex-col gap-y-6 bg-white text-sm p-6 h-60 w-80"
+      className="shadow-xl border-t-4 border-green-400 flex flex-col gap-y-5 bg-white text-sm p-6 h-72 w-80"
     >
-      <div className="text-3xl font-bold text-gray-900">
-        <div className="text-xs font-normal text-gray-400">
-          Subvention Fonds Vert / total des dépenses
+      <div className="text-2xl font-bold text-gray-900">
+        <div className="text-xs font-normal text-gray-500 pb-1 mb-1 border-b border-gray-100">
+          Subvention Fonds Vert
         </div>
-        {getSubvention(projet)}{" "}
-        <span className="text-base font-medium">
-          / {formattedMontantTotalDepense}{" "}
-        </span>
+        <div className="flex justify-between">
+          <div>{getSubvention(projet)}</div>
+          {ratio && <div className="text-gray-400 opacity-80">{ratio}%</div>}
+        </div>
+      </div>
+      <div className="text-base font-medium">
+        <div className="text-xs font-normal text-gray-500 pb-1 mb-1 border-b border-gray-100">
+          Montant total du projet
+        </div>
+        {formattedMontantTotalDepense}
       </div>
       <div className="h-full flex flex-col justify-between">
-        <p className="mb-0 line-clamp-4 text-sm text-gray-600">
+        <p className="mb-0 line-clamp-4 text-sm text-gray-600 leading-tight">
           {projet.nom_du_projet}
         </p>
-        <p className="mb-0 text-right text-xs text-gray-400">
+        <p className="mb-0 text-right text-xs text-gray-500">
           {projet.nom_commune}
         </p>
       </div>
