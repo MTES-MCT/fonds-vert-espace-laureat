4b24f309a9c66b6f3b7169191d876dee76364eb2
Cédric Soulas <soulascedric@gmail.com>
2025-02-03 20:11:06 +0100

    chore: move impact details to specific components

---
+++ app/espace-laureat/_components/dossier-section/details/impact-details/ImpactSubmission.tsx
@@ -0,0 +1,61 @@
+import Link from "next/link";
+
+import { Help } from "@/app/espace-laureat/_components/dossier-section/details/impact-details/Help";
+import { Metrics } from "@/services/fondsvert/dossier";
+import { fetchPrefillMapping } from "@/services/grist/impact";
+import { requireEnv } from "@/utils/env";
+
+export async function ImpactSubmission({ metriques }: { metriques: Metrics }) {
+  const [dsImpactUrl] = requireEnv("DS_IMPACT_URL");
+  const prefillMapping = await fetchPrefillMapping();
+
+  // Pour le moment, les sous-mesures (value de type object) ne sont pas supportées
+  const metricEntries = Object.entries(metriques).filter(
+    (entry): entry is [string, string | number] =>
+      ["string", "number"].includes(typeof entry[1]),
+  );
+
+  const prefilledDsImpactUrl = new URL(dsImpactUrl);
+
+  for (const [metricKey, value] of metricEntries) {
+    const champId = prefillMapping[metricKey];
+    if (champId) {
+      prefilledDsImpactUrl.searchParams.append(
+        `champ_${champId}`,
+        String(value),
+      );
+    }
+  }
+
+  return (
+    <>
+      <Help>
+        Les données de votre projet participent à la transition écologique.
+        Merci de compléter l'évaluation d'impact réel de votre projet,
+        conformément aux engagements liés à la subvention.
+      </Help>
+      {metricEntries.length > 0 && (
+        <>
+          <p>
+            Pour rappel, voici les metriques que vous avez renseignées lors de
+            la demande de subvention :
+          </p>
+          <ul>
+            {metricEntries.map(([key, value]) => (
+              <li key={key}>
+                {key}: <strong>{value}</strong>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+      <Link
+        className="fr-btn fr-btn--tertiary fr-btn--sm bg-white hover:bg-gray-50"
+        target="_blank"
+        href={prefilledDsImpactUrl.toString()}
+      >
+        Compléter l'évaluation
+      </Link>
+    </>
+  );
+}
