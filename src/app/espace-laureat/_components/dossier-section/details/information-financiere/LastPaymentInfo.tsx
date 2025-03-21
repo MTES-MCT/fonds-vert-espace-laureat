d50df44074e51eaf490c69012e2b286e38782511
Cédric Soulas <soulascedric@gmail.com>
2025-03-21 17:26:04 +0100

    chore: split information financiere into two new components

---
+++ src/app/espace-laureat/_components/dossier-section/details/information-financiere/LastPaymentInfo.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+
+interface Payment {
+  montant_paye: number;
+  date_dp: string;
+  numero_dp: string;
+}
+
+interface Engagement {
+  annee: number;
+  montant_engage: number;
+  demandes_paiement: {
+    numero_dp: string;
+    date_dp: string;
+    montant_paye: number;
+  }[];
+}
+
+interface GroupedEngagement {
+  numero_ej: string;
+  montant_engage_initial: number;
+  historique: Engagement[];
+}
+
+interface LastPaymentInfoProps {
+  group: GroupedEngagement;
+  formatMontant: (montant: number) => string;
+  formatDate: (date: string) => string;
+}
+
+export function LastPaymentInfo({
+  group,
+  formatMontant,
+  formatDate,
+}: LastPaymentInfoProps) {
+  const getLastPayment = (group: GroupedEngagement) => {
+    return group.historique
+      .flatMap((item) => item.demandes_paiement)
+      .reduce(
+        (mostRecent, current) =>
+          !mostRecent || current.date_dp > mostRecent.date_dp
+            ? current
+            : mostRecent,
+        null as null | Payment,
+      );
+  };
+
+  const lastPayment = getLastPayment(group);
+
+  if (!lastPayment) {
+    return (
+      <div className="text-sm text-gray-400 font-medium mb-4">
+        Aucun paiement n'a été réalisé pour ce projet.
+      </div>
+    );
+  }
+
+  return (
+    <dl className="flex flex-wrap text-sm gap-x-8 gap-y-4 mb-8">
+      <div>
+        <dt>Dernier paiement</dt>
+        <dd>{formatMontant(lastPayment.montant_paye)}</dd>
+      </div>
+      <div>
+        <dt>Date de paiement</dt>
+        <dd>{formatDate(lastPayment.date_dp)}</dd>
+      </div>
+      <div>
+        <dt>Numéro de demande</dt>
+        <dd>{lastPayment.numero_dp}</dd>
+      </div>
+    </dl>
+  );
+}
