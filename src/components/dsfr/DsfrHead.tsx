462ebe39cb76c44b47cae60b52976bcb8f81c3a7
Cédric Soulas <soulascedric@gmail.com>
2025-04-11 11:55:12 +0200

    feat: preload Marianne fonts to improve loading experience

--- src/components/dsfr/DsfrHead.tsx
+++ src/components/dsfr/DsfrHead.tsx
@@ -1,8 +1,17 @@
 import AppleTouchIcon from "@codegouvfr/react-dsfr/dsfr/favicon/apple-touch-icon.png";
 import FaviconIco from "@codegouvfr/react-dsfr/dsfr/favicon/favicon.ico";
 import FaviconSvg from "@codegouvfr/react-dsfr/dsfr/favicon/favicon.svg";
+import marianneBoldWoff2Url from "@codegouvfr/react-dsfr/dsfr/fonts/Marianne-Bold.woff2";
+import marianneMediumWoff2Url from "@codegouvfr/react-dsfr/dsfr/fonts/Marianne-Medium.woff2";
+import marianneRegularWoff2Url from "@codegouvfr/react-dsfr/dsfr/fonts/Marianne-Regular.woff2";
 import { getAssetUrl } from "@codegouvfr/react-dsfr/tools/getAssetUrl";
 import React from "react";
+
+const preloadedFont = [
+  marianneRegularWoff2Url,
+  marianneMediumWoff2Url,
+  marianneBoldWoff2Url,
+];
 
 export function DsfrHead() {
   return (
@@ -14,6 +23,16 @@ export function DsfrHead() {
         href={getAssetUrl(FaviconIco)}
         type="image/x-icon"
       />
+      {preloadedFont.map((url) => (
+        <link
+          key={url}
+          href={url}
+          rel="preload"
+          type="font/woff2"
+          as="font"
+          crossOrigin="anonymous"
+        />
+      ))}
     </>
   );
 }
