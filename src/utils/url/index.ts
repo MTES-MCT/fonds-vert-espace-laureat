4607000907b85980dcdcb1a3cee346d07963e398
Cédric Soulas <soulascedric@gmail.com>
2025-07-25 12:37:17 +0200

    fix: fix returnTo logic not using baseUrl

--- src/utils/url/index.ts
+++ src/utils/url/index.ts
@@ -2,8 +2,12 @@ import { requireEnv } from "@/utils/env";
 
 const [baseUrl] = requireEnv("NEXT_PUBLIC_BASE_URL");
 
-export const welcomeUrl = new URL("/", baseUrl);
+export const buildUrl = (path: string): URL => {
+  return new URL(path, baseUrl);
+};
 
-export const dashboardUrl = new URL("/espace-laureat", baseUrl);
+export const welcomeUrl = buildUrl("/");
 
-export const errorUrl = new URL("/connexion/echec", baseUrl).toString();
+export const dashboardUrl = buildUrl("/espace-laureat");
+
+export const errorUrl = buildUrl("/connexion/echec");
