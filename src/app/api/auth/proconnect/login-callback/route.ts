995a315047bdeadf51de6daac92c3cde4743c44d
Cédric Soulas <soulascedric@gmail.com>
2025-07-25 12:37:17 +0200

    fix: fix returnTo logic not using baseUrl

--- src/app/api/auth/proconnect/login-callback/route.ts
+++ src/app/api/auth/proconnect/login-callback/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import { proConnectAuthenticate } from "@/services/proconnect";
 import { logException } from "@/utils/error";
 import { setSession, withSession } from "@/utils/session";
-import { dashboardUrl, errorUrl } from "@/utils/url";
+import { buildUrl, dashboardUrl, errorUrl } from "@/utils/url";
 
 export const GET = withSession(async function loginCallbackRoute(req) {
   try {
@@ -16,8 +16,7 @@ export const GET = withSession(async function loginCallbackRoute(req) {
       await req.session.save();
 
       if (returnTo.startsWith("/") && !returnTo.startsWith("//")) {
-        const returnUrl = new URL(returnTo, req.url);
-        return NextResponse.redirect(returnUrl);
+        return NextResponse.redirect(buildUrl(returnTo));
       }
     }
 
